abedd85c621b68084b05c9ccadf44de860f4f13d
ChrisHAdams <chris.adams@lucentitservices.co.uk>
2018-03-27 22:56:05 +0100

    Updates

--- src/healthcheck/sendEmails.js
+++ src/healthcheck/sendEmails.js
@@ -1,15 +1,15 @@
-var config = require('config');
+let config = require('config');
 const mailer = require('nodemailer');
 
 function sendEmail(items, log){
-  
+
   let smtp = mailer.createTransport(config.get('mailTransport'));
   let mailDetails = config.get('mailDetails');
   let mailOptions = {"to": mailDetails.to,
                      "from" : mailDetails.from,
                      "subject": mailDetails.subject,
                      "html": getHtml(items)};
-                    
+
   trySend(3, err => !err ? log.info('Alert email sent') : log.error('Alert email could not be sent', err));
 
   function trySend(tries, callback, log) {
@@ -27,11 +27,11 @@ function sendEmail(items, log){
 }
 
 function getAlertText(items) {
-  
+
   let returnArray = [];
-  
+
   for (var i = 0; i < items.length; i++) {
-    
+
     let reportItem = false;
     let item = items[i];
     if (item.responseDetails.hasOwnProperty('responseCodeCheck')) {
@@ -39,31 +39,31 @@ function getAlertText(items) {
         reportItem = true;
       }
     }
-   
+
     if (item.responseDetails.hasOwnProperty('responseTimeCheck')) {
       if(item.responseDetails.responseTimeCheck == 'Fail') {
         reportItem = true;
-      }      
+      }
     }
-    
+
     if (reportItem){
       let lineObj =  {"header": `${item.name} check FAILED`,
                       "description": `    ${item.description}`,
                       "statusCheck": `        Status Code - received ${item.responseDetails.responseCode}.  Expected ${item.expectedResults.expectedStatusCode}.`,
                       "timeCheck": `          Response Time - ${item.responseDetails.responseTime}ms.  Expected < ${item.expectedResults.expectedResponseTime}ms`};
-      
+
       returnArray.push(lineObj);
     }
   }
-  
+
   return returnArray;
 
 }
 
 function getHtml(items) {
-  
+
   let dataArray = getAlertText(items);
-  
+
   let htmlString = `
   <html>
     <head>
@@ -79,7 +79,7 @@ function getHtml(items) {
       ${dataArray.map (item=> `<p>${item.header}<br/>${item.description}<br/>${item.statusCheck}<br/>${item.timeCheck}</p>`)}
     </body>
   </html>`;
-  
+
   return htmlString;
 }
 
